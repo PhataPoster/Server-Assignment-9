39f14878914fa3b7b065670dffb97f168e0268d6
Ashikur Rahman <ashikurrahman3060@gamil.com>
2026-05-20 00:36:58 +0600

    write get mathod for fatching doctors

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
-const port = 5000;
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("docAppoint");
+    const doctorsCollection = db.collection('doctor');
+
+    app.get('/doctors', async (req, res) => {
+        const cursor = doctorsCollection.find();
+        const doctors = await cursor.toArray();
+        res.send(doctors);
+    })
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
